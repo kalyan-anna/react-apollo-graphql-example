57d15ec5c149f7773493140b43f463f8a7318187
Kalyan A <kalyan.shelf@gmail.com>
2025-02-02 15:36:40 +1000

    retry link

--- jura-client/src/utils/client.helper.ts
+++ jura-client/src/utils/client.helper.ts
@@ -1,10 +1,11 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
 
-import { accessTokenAtom } from "../state/auth";
-import { getDefaultStore } from "jotai";
-import { gqlGlobalErrorAtom } from "../state/error";
-import { onError } from "@apollo/client/link/error";
 import { setContext } from "@apollo/client/link/context";
+import { onError } from "@apollo/client/link/error";
+import { RetryLink } from "@apollo/client/link/retry";
+import { getDefaultStore } from "jotai";
+import { accessTokenAtom } from "../state/auth";
+import { gqlGlobalErrorAtom } from "../state/error";
 
 const httpLink = createHttpLink({
   uri: "http://localhost:3000/graphql",
@@ -22,39 +23,35 @@ const authLink = setContext(async (_, { headers }) => {
   };
 });
 
-const errorLink = onError(
-  ({ graphQLErrors, networkError, operation, forward }) => {
-    const store = getDefaultStore();
+const errorLink = onError(({ graphQLErrors, networkError, operation, forward }) => {
+  const store = getDefaultStore();
 
-    if (graphQLErrors) {
-      graphQLErrors.forEach(({ message, locations, path }) =>
-        console.error(
-          `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
-        )
-      );
-      const shouldThrowError = !graphQLErrors.some(
-        (error) => error.extensions?.code === "BAD_USER_INPUT"
-      );
+  if (graphQLErrors) {
+    graphQLErrors.forEach(({ message, locations, path }) =>
+      console.error(`[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`)
+    );
+    const shouldThrowError = !graphQLErrors.some((error) => error.extensions?.code === "BAD_USER_INPUT");
 
-      if (shouldThrowError) {
-        const messages = graphQLErrors.map((error) => error.message).join(", ");
-        store.set(gqlGlobalErrorAtom, messages);
-        return;
-      }
-    }
-
-    if (networkError) {
-      console.error(`[Network error]: ${networkError}`);
-      store.set(gqlGlobalErrorAtom, networkError.message);
+    if (shouldThrowError) {
+      const messages = graphQLErrors.map((error) => error.message).join(", ");
+      store.set(gqlGlobalErrorAtom, messages);
       return;
     }
-
-    return forward(operation);
   }
-);
+
+  if (networkError) {
+    console.error(`[Network error]: ${networkError}`);
+    store.set(gqlGlobalErrorAtom, networkError.message);
+    return;
+  }
+
+  return forward(operation);
+});
+
+const retryLink = new RetryLink({ attempts: { max: 2 } });
 
 export const client = new ApolloClient({
-  link: authLink.concat(errorLink).concat(httpLink),
+  link: retryLink.concat(authLink).concat(errorLink).concat(httpLink),
   cache: new InMemoryCache(),
   defaultOptions: {
     mutate: {
